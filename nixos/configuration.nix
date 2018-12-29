bf720894c9c2845669b9cfb8706bbf0174fe6550
Roman Joost <roman@bromeco.de>
2018-12-29 13:32:29 +1000

    Nixos and user level config

---
+++ nixos/configuration.nix
@@ -0,0 +1,140 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    }
+  ];
+
+  networking.networkmanager.enable = true;
+  networking.hostName = "krombopulos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_AU.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Australia/Brisbane";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    firefox
+    git
+    nix-prefetch-scripts
+    emacs
+    wget
+    vim
+    arandr
+    evince
+    silver-searcher
+    hicolor-icon-theme
+    gnome3.adwaita-icon-theme
+    getmail
+    maildrop
+    gnupg
+    powertop
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.syncthing.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  services.printing = {
+   enable = true;
+   browsing = true;
+   drivers = [ pkgs.brlaser ];
+  };
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.bluetooth.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    desktopManager.default = "none";
+    desktopManager.xterm.enable = false;
+    desktopManager.xfce.enable = true;
+    displayManager.lightdm.enable = true;
+    xkbOptions = "crtl:nocaps";
+
+    windowManager.default = "xmonad";
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages : [haskellPackages.xmobar];
+    };
+  };
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.rjoost = {
+      createHome = true;
+      extraGroups = ["wheel" "video" "audio" "disk" "networkmanager"];
+      group = "rjoost";
+      home = "/home/rjoost";
+      isNormalUser = true;
+      uid = 1000;
+      shell = pkgs.zsh;
+  };
+
+  users.groups.rjoost.gid = 1000;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+}
---
+++ nixpkgs/config.nix
@@ -0,0 +1,68 @@
+{ pkgs }:
+
+{ packageOverrides = super: let self = super.pkgs; in with self; rec {
+## My one-shot install environment.
+    my-meta-env = buildEnv {
+      name = "meta-env";
+      paths = [
+        haskell-env
+      ];
+    };
+
+    ## Haskell environment
+    my-haskell-env = buildEnv {
+      name = "haskell-env";
+      paths = with pkgs.haskellPackages; [
+        cabal-install
+        ghcid
+        hlint
+        hindent
+        hasktags
+        ctags
+        ispell
+      ];
+    };
+
+    my-media = buildEnv {
+      name = "media";
+      paths = [
+        pavucontrol
+      ];
+    };
+
+    my-games = buildEnv {
+      name = "games";
+      paths = [
+        minetest
+      ];
+    };
+
+    my-wm = buildEnv {
+      name = "wm";
+      paths = [
+        xlockmore
+        xorg.xbacklight
+        pinentry
+        tor-browser-bundle-bin
+        noto-fonts-emoji
+        feh
+        termonad-with-packages
+        gsettings_desktop_schemas
+        gimp
+        python36Packages.syncthing-gtk
+      ];
+    };
+
+    my-console = buildEnv {
+      name = "console";
+      paths = [
+        tmux
+        poppler_utils
+        aspellDicts.en
+        file
+        python35
+        lsof
+      ];
+    };
+  };
+}
