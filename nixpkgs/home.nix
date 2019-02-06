61ab675bc0b530eb2e4a19b035d234184b9c2277
Roman Joost <roman@bromeco.de>
2019-02-06 19:15:52 +1000

    Haskell packages are not needed any more
    The can now be installed with a specific package

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -19,16 +19,11 @@ home.packages = [
   pkgs.evince
   pkgs.feh
   pkgs.file
-  pkgs.ghc
   pkgs.gimp
   pkgs.git
   pkgs.gnome3.cheese
   pkgs.gnupg
   pkgs.gsettings_desktop_schemas
-  pkgs.haskellPackages.ghcid
-  pkgs.haskellPackages.hasktags
-  pkgs.haskellPackages.hindent
-  pkgs.haskellPackages.hlint
   pkgs.ispell
   pkgs.killall
   pkgs.lsof
@@ -49,22 +44,7 @@ home.packages = [
   pkgs.xlockmore
   pkgs.xorg.xbacklight
   pkgsUnstable.firefox
-  # Was not installable:
-  #
-  # Configuring heterocephalus-1.0.5.2...
-  # CallStack (from HasCallStack):
-  #   die', called at libraries/Cabal/Cabal/Distribution/Simple/Configure.hs:950:20 in Cabal-2.4.0.1:Distribution.Simple.Configure
-  #   configureFinalizedPackage, called at libraries/Cabal/Cabal/Distribution/Simple/Configure.hs:460:12 in Cabal-2.4.0.1:Distribution.Simple.Configure
-  #   configure, called at libraries/Cabal/Cabal/Distribution/Simple.hs:596:20 in Cabal-2.4.0.1:Distribution.Simple
-  #   confHook, called at libraries/Cabal/Cabal/Distribution/Simple/UserHooks.hs:67:5 in Cabal-2.4.0.1:Distribution.Simple.UserHooks
-  #   configureAction, called at libraries/Cabal/Cabal/Distribution/Simple.hs:178:19 in Cabal-2.4.0.1:Distribution.Simple
-  #   defaultMainHelper, called at libraries/Cabal/Cabal/Distribution/Simple.hs:115:27 in Cabal-2.4.0.1:Distribution.Simple
-  #   defaultMain, called at Setup.hs:2:8 in main:Main
-  # Setup: Encountered missing dependencies:
-  # containers ==0.5.*
-  #
-  # Installed via nix-env from termonad git checkout HEAD
-  # pkgsUnstable.termonad-with-packages
+  pkgsUnstable.termonad-with-packages
 ];
 
 services.syncthing = {
