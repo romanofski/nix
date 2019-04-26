7ad513781f5a0a44048a0ba13e4c6c5e317fc533
Roman Joost <roman@bromeco.de>
2019-04-26 18:03:34 +1000

    Reformat

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -11,66 +11,66 @@ let
   ) { };
   opts = import ./opts.nix;
 in {
-home.packages = [
-  pkgs.ansible
-  pkgs.arandr
-  pkgs.aspellDicts.en
-  pkgs.bind
-  pkgs.binutils
-  pkgs.blueman
-  pkgs.bluez
-  pkgs.bluez-tools
-  pkgs.ctags
-  pkgs.elinks
-  pkgs.emacs
-  pkgs.evince
-  pkgs.feh
-  pkgs.file
-  pkgs.gdb
-  pkgs.gimp
-  pkgs.git
-  pkgs.gnome3.cheese
-  pkgs.gnupg
-  pkgs.gsettings_desktop_schemas
-  pkgs.inkscape
-  pkgs.ispell
-  pkgs.killall
-  pkgs.libreoffice-fresh
-  pkgs.lsof
-  pkgs.minetest
-  pkgs.noto-fonts-emoji
-  pkgs.openjdk8
-  pkgs.openttd
-  pkgs.parallel
-  pkgs.pass
-  pkgs.pavucontrol
-  pkgs.pinentry
-  pkgs.poppler_utils
-  pkgs.powertop
-  pkgs.python36Packages.syncthing-gtk
-  pkgs.silver-searcher
-  pkgs.sshpass
-  pkgs.tmux
-  pkgs.torbrowser
-  pkgs.unzip
-  pkgs.vim
-  pkgs.wget
-  pkgs.wine
-  pkgs.xlockmore
-  pkgs.xorg.xbacklight
-  pkgsUnstable.openra
-  pkgsUnstable.firefox
-];
+  home.packages = [
+    pkgs.ansible
+    pkgs.arandr
+    pkgs.aspellDicts.en
+    pkgs.bind
+    pkgs.binutils
+    pkgs.blueman
+    pkgs.bluez
+    pkgs.bluez-tools
+    pkgs.ctags
+    pkgs.elinks
+    pkgs.emacs
+    pkgs.evince
+    pkgs.feh
+    pkgs.file
+    pkgs.gdb
+    pkgs.gimp
+    pkgs.git
+    pkgs.gnome3.cheese
+    pkgs.gnupg
+    pkgs.gsettings_desktop_schemas
+    pkgs.inkscape
+    pkgs.ispell
+    pkgs.killall
+    pkgs.libreoffice-fresh
+    pkgs.lsof
+    pkgs.minetest
+    pkgs.noto-fonts-emoji
+    pkgs.openjdk8
+    pkgs.openttd
+    pkgs.parallel
+    pkgs.pass
+    pkgs.pavucontrol
+    pkgs.pinentry
+    pkgs.poppler_utils
+    pkgs.powertop
+    pkgs.python36Packages.syncthing-gtk
+    pkgs.silver-searcher
+    pkgs.sshpass
+    pkgs.tmux
+    pkgs.torbrowser
+    pkgs.unzip
+    pkgs.vim
+    pkgs.wget
+    pkgs.wine
+    pkgs.xlockmore
+    pkgs.xorg.xbacklight
+    pkgsUnstable.openra
+    pkgsUnstable.firefox
+  ];
 
-services.syncthing = {
-  enable = true;
-};
+  services.syncthing = {
+    enable = true;
+  };
   services.gpg-agent.enable = true;
 
   accounts.email = {
     accounts = {
       bromeco = {
-      realName = "Róman Joost";
+        realName = "Róman Joost";
         address = "roman@bromeco.de";
         userName = "${opts.username}";
         primary = true;
@@ -80,7 +80,7 @@ services.syncthing = {
         smtp.tls.useStartTls = true;
         passwordCommand = "${pkgs.pass}/bin/pass bromeco";
         msmtp = {
-         enable = true;
+          enable = true;
         };
       };
     };
