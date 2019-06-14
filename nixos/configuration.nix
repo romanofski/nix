c5cee75107480c1795d1f0ef1c3db63c468b14d2
Róman Joost <roman@bromeco.de>
2019-06-14 10:14:05 +1000

    Fix indentation

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,77 +7,77 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./yubikey.nix
-      ./printing.nix
-      ./virtualisation.nix
-      ./firewall.nix
+    ./hardware-configuration.nix
+    ./yubikey.nix
+    ./printing.nix
+    ./virtualisation.nix
+    ./firewall.nix
     ];
 
-  networking.networkmanager.enable = true;
-  networking.hostName = "krombopulos"; # Define your hostname.
+    networking.networkmanager.enable = true;
+    networking.hostName = "krombopulos"; # Define your hostname.
 
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_AU.UTF-8";
-  };
-
-  # Set your time zone.
-  time.timeZone = "Australia/Brisbane";
-
-  # Autocompletion for zsh
-  environment.pathsToLink = [ "/share/zsh "];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    nix-prefetch-scripts
-    linuxPackages.tp_smapi
-    home-manager
-    git
-  ];
-
-  services.openssh.enable = true;
-  services.tlp.enable = true;
-  services.tor.enable = true;
-  services.tor.client.enable = true;
-  services.dbus.packages = with pkgs; [ gnome3.dconf ];
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-  };
-
-  hardware.bluetooth.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    xkbModel = "pc105";
-    desktopManager.default = "none";
-    desktopManager.xterm.enable = false;
-    desktopManager.xfce.enable = true;
-    displayManager.lightdm.enable = true;
-    xkbOptions = "compose:ralt";
-
-    windowManager.default = "xmonad";
-    windowManager.xmonad = {
-      enable = true;
-      enableContribAndExtras = true;
-      extraPackages = haskellPackages : [haskellPackages.xmobar];
+    # Select internationalisation properties.
+    i18n = {
+      consoleFont = "Lat2-Terminus16";
+      consoleKeyMap = "us";
+      defaultLocale = "en_AU.UTF-8";
     };
-  };
 
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
+    # Set your time zone.
+    time.timeZone = "Australia/Brisbane";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+    # Autocompletion for zsh
+    environment.pathsToLink = [ "/share/zsh "];
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      nix-prefetch-scripts
+      linuxPackages.tp_smapi
+      home-manager
+      git
+    ];
+
+    services.openssh.enable = true;
+    services.tlp.enable = true;
+    services.tor.enable = true;
+    services.tor.client.enable = true;
+    services.dbus.packages = with pkgs; [ gnome3.dconf ];
+
+    # Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+    hardware.opengl = {
+      enable = true;
+      driSupport32Bit = true;
+    };
+
+    hardware.bluetooth.enable = true;
+
+    # Enable the X11 windowing system.
+    services.xserver = {
+      enable = true;
+      layout = "us";
+      xkbModel = "pc105";
+      desktopManager.default = "none";
+      desktopManager.xterm.enable = false;
+      desktopManager.xfce.enable = true;
+      displayManager.lightdm.enable = true;
+      xkbOptions = "compose:ralt";
+
+      windowManager.default = "xmonad";
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        extraPackages = haskellPackages : [haskellPackages.xmobar];
+      };
+    };
+
+    # Enable touchpad support.
+    services.xserver.libinput.enable = true;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.rjoost = {
       createHome = true;
       extraGroups = ["wheel" "video" "audio" "disk" "networkmanager"];
@@ -91,7 +91,7 @@
     users.groups.rjoost.gid = 1000;
 
     # This value determines the NixOS release with which your system is to be
-      # compatible, in order to avoid breaking some software such as database
+    # compatible, in order to avoid breaking some software such as database
     # servers. You should change this only after NixOS release notes say you
     # should.
     system.stateVersion = "19.03"; # Did you read the comment?
