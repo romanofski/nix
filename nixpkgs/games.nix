d9422b07e04a35586a7e786761703c23a3f34684
Róman Joost <roman@bromeco.de>
2019-09-26 18:18:57 +1000

    Games: Install dwarf-fortress

--- nixpkgs/games.nix
+++ nixpkgs/games.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 
 let
+  nixpkgs = import <nixos> { };
   pkgsUnstable = import <unstable> { };
 in {
+  nixpkgs.config.allowUnfree = true;
   home.packages = [
     pkgs.endless-sky
     pkgs.minetest
+    pkgs.dwarf-fortress-packages.dwarf-fortress-full
     pkgs.openttd
     pkgs.wine
     pkgsUnstable.openra
