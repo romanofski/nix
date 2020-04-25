72d8bc708c065ed442b8171a22980da5694612d7
Róman Joost <roman@bromeco.de>
2020-04-25 11:15:11 +1000

    Reformat based on nixfmt

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,72 +7,72 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      <nixos-hardware/lenovo/thinkpad/t480s>
-      ./hardware-configuration.nix
-      ./yubikey.nix
-      ./printing.nix
-      ./virtualisation.nix
-      ./firewall.nix
-      ./unfree.nix
-      ./services/ntp.nix
-      ./services/tor.nix
-      ./services/upower.nix
-      ./services/gvfs.nix
+    <nixos-hardware/lenovo/thinkpad/t480s>
+    ./hardware-configuration.nix
+    ./yubikey.nix
+    ./printing.nix
+    ./virtualisation.nix
+    ./firewall.nix
+    ./unfree.nix
+    ./services/ntp.nix
+    ./services/tor.nix
+    ./services/upower.nix
+    ./services/gvfs.nix
     ];
 
-  systemd.tmpfiles.rules = [ "d /tmp 1777 root root 10d"];
+    systemd.tmpfiles.rules = [ "d /tmp 1777 root root 10d"];
 
-  networking.networkmanager.enable = true;
-  networking.hostName = "krombopulos"; # Define your hostname.
+    networking.networkmanager.enable = true;
+    networking.hostName = "krombopulos"; # Define your hostname.
 
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_AU.UTF-8";
-  };
-
-  # Set your time zone.
-  time.timeZone = "Australia/Brisbane";
-
-  # Autocompletion for zsh
-  environment.pathsToLink = [ "/share/zsh "];
-  # Use this path as otherwise $PATH on SSH is set to /usr/bin et.al
-  environment.sessionVariables = { PATH="/run/current-system/sw/bin"; };
-
-  environment.systemPackages = with pkgs; [
-    nix-prefetch-scripts
-    linuxPackages.tp_smapi
-    home-manager
-    git
-  ];
-
-  services.openssh.enable = true;
-  services.dbus.packages = with pkgs; [ gnome3.dconf ];
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    xkbModel = "pc105";
-    desktopManager.default = "xfce";
-    desktopManager.xterm.enable = false;
-    desktopManager.xfce = {
-      enable = true;
-      noDesktop = true;
-      enableXfwm = true;
+    # Select internationalisation properties.
+    i18n = {
+      consoleFont = "Lat2-Terminus16";
+      consoleKeyMap = "us";
+      defaultLocale = "en_AU.UTF-8";
     };
-    displayManager.lightdm.enable = true;
-    xkbOptions = "compose:ralt";
-    exportConfiguration = true;
-    extraConfig = builtins.readFile ./xorg.conf;
-    videoDrivers = ["intel" "vesa"];
-  };
 
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
+    # Set your time zone.
+    time.timeZone = "Australia/Brisbane";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+    # Autocompletion for zsh
+    environment.pathsToLink = [ "/share/zsh "];
+    # Use this path as otherwise $PATH on SSH is set to /usr/bin et.al
+    environment.sessionVariables = { PATH="/run/current-system/sw/bin"; };
+
+    environment.systemPackages = with pkgs; [
+      nix-prefetch-scripts
+      linuxPackages.tp_smapi
+      home-manager
+      git
+    ];
+
+    services.openssh.enable = true;
+    services.dbus.packages = with pkgs; [ gnome3.dconf ];
+
+    # Enable the X11 windowing system.
+    services.xserver = {
+      enable = true;
+      layout = "us";
+      xkbModel = "pc105";
+      desktopManager.default = "xfce";
+      desktopManager.xterm.enable = false;
+      desktopManager.xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = true;
+      };
+      displayManager.lightdm.enable = true;
+      xkbOptions = "compose:ralt";
+      exportConfiguration = true;
+      extraConfig = builtins.readFile ./xorg.conf;
+      videoDrivers = ["intel" "vesa"];
+    };
+
+    # Enable touchpad support.
+    services.xserver.libinput.enable = true;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.rjoost = {
       createHome = true;
       extraGroups = ["wheel" "video" "audio" "disk" "networkmanager"];
@@ -86,7 +86,7 @@
     users.groups.rjoost.gid = 1000;
 
     # This value determines the NixOS release with which your system is to be
-      # compatible, in order to avoid breaking some software such as database
+    # compatible, in order to avoid breaking some software such as database
     # servers. You should change this only after NixOS release notes say you
     # should.
     system.stateVersion = "19.03"; # Did you read the comment?
