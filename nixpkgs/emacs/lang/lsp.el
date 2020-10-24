a52b3921d63e2d0285c0e27ea7a804a47ec5dfdd
Róman Joost <roman@bromeco.de>
2020-10-24 13:47:45 +1000

    Additional tweaks for lsp-mode
    ... and I don't know what I'm doing ...

--- nixpkgs/emacs/lang/lsp.el
+++ nixpkgs/emacs/lang/lsp.el
@@ -5,22 +5,32 @@
          (web-mode . lsp-deferred))
   :config
   (setq-default lsp-prefer-flymake nil
-                lsp-auto-guess-root t)
-  (bind-key* "M-." #'lsp-goto-type-definition lsp-mode-map))
+                lsp-auto-guess-root t
+                lsp-enable-snippet t
+                lsp-idle-delay 0.500))
 
 (use-package company-lsp
+  :after company lsp-mode
   :config
+  (setq company-lsp-enable-snippet t)
+  :init
   (push 'company-lsp company-backends))
 
 (use-package lsp-ui
   :init
-  (setq-default lsp-ui-flycheck-enable t)
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-use-webkit nil
+        lsp-ui-doc-position 'at-point
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-diagnostics nil
+        lsp-ui-sideline-ignore-duplicate t
+        lsp-ui-sideline-delay 0.7)
   :hook ((lsp-mode . flycheck-mode)
          (lsp-mode . lsp-ui-mode))
   :config
+  (setq lsp-ui-flycheck-enable t)
   (flycheck-add-mode 'javascript-eslint 'web-mode)
-  (flycheck-add-next-checker 'lsp-ui 'javascript-eslint)
-
-  (bind-key "M-U" #'lsp-ui-peek-find-references lsp-mode-map))
+  (flycheck-add-next-checker 'lsp-ui 'javascript-eslint))
 
 (provide 'use-lsp)
