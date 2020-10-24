348d92a99a06210b441a35f0c4861c0775ac8862
Róman Joost <roman@bromeco.de>
2020-10-24 13:43:33 +1000

    Avoid locales error
    Without this package installed, we run into locales problems on
    non-NixOS systems. E.g. starting xmobar with UTF-8 characters results
    in:
    
       hGetContents: invalid argument (invalid byte sequence)
    
    Installing those locales fixes this problem.

--- nixpkgs/programs/zsh.nix
+++ nixpkgs/programs/zsh.nix
@@ -9,6 +9,15 @@ in with secrets; {
     shellAliases = {
       c = "cd ..";
     };
+    loginExtra = ''
+      [ -z "$LOCALE_ARCHIVE" ] && LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive"
+      if env | grep -q ^LOCALE_ARCHIVE=
+      then
+        echo "LOCALE_ARCHIVE already set by OS"
+      else
+        export LOCALE_ARCHIVE
+      fi
+    '';
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "sudo"];
