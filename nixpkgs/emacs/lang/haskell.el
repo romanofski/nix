4983f015c193bc5ca8a55134dfa1aef261d1083c
Róman Joost <roman@bromeco.de>
2020-12-07 17:11:59 +1000

    Don't think haskell-mode is needed any more
    Replaced in favour of lsp-haskell

--- nixpkgs/emacs/lang/haskell.el
+++ nixpkgs/emacs/lang/haskell.el
@@ -1,25 +1,8 @@
-(use-package haskell-mode
-  :mode "\\.\\(hs\\|lhs\\|hsc\\|cpphs\\|c2hs\\)\\'"
-  :init
-  (add-hook 'haskell-mode-hook 'fira-code-mode)
-  :general
-  (general-nvmap :keymaps 'haskell-mode-map
-                 "gI" 'haskell-navigate-imports)
-
-  (general-nmap :keymaps 'haskell-mode-map
-                :prefix ","
-                "e" '(:ignore t :which-key "edit")
-                "ei" 'haskell-mode-format-imports)
-  )
 (use-package lsp-haskell
   :ensure t
-  :init
+  :config
   (setq lsp-haskell-process-path-hie "haskell-language-server")
   :hook
   (haskell-mode . lsp)
   (haskell-literate-mode . lsp)
-  )
-
-(use-package flycheck-haskell
-  :ensure t
   )
