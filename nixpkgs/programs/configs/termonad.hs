e8910bde8d6c399d09f106ef6981af450f5f95fd
Róman Joost <roman@bromeco.de>
2021-01-26 14:21:10 +1000

    Choose the dark solarized theme
    I always used it but changed at some point in time. Purebred only ships
    with dark (since I'm too lazy to maintain two themes at this
    point). Since it's hard to use with a light theme, switch back to dark.

--- nixpkgs/programs/configs/termonad.hs
+++ nixpkgs/programs/configs/termonad.hs
@@ -14,7 +14,7 @@ import Termonad.Config.Colour
   ( AlphaColour, ColourConfig, Palette(ExtendedPalette), addColourExtension
   , createColour, createColourExtension, defaultColourConfig
   , foregroundColour, backgroundColour, palette, mkList8, List8
-  , defaultLightColours
+  , defaultLightColours, defaultStandardColours
   )
 import Data.Maybe (fromMaybe)
 import Control.Lens
@@ -72,6 +72,41 @@ solarizedLight =
       , createColour 253 246 227 -- base3
       ]
 
+-- This is our Solarized dark 'ColourConfig'.  It holds all of our dark-related settings.
+solarizedDark :: ColourConfig (AlphaColour Double)
+solarizedDark =
+  defaultColourConfig
+    -- Set the default foreground colour of text of the terminal.
+    { foregroundColour = Set (createColour 131 148 150) -- base0
+    , backgroundColour = Set (createColour   0  43  54) -- base03
+    -- Set the extended palette that has 2 Vecs of 8 Solarized palette colours
+    , palette = ExtendedPalette solarizedDark1 solarizedDark2
+    }
+  where
+    solarizedDark1 :: List8 (AlphaColour Double)
+    solarizedDark1 = fromMaybe defaultStandardColours $ mkList8
+      [ createColour   7  54  66 -- base02
+      , createColour 220  50  47 -- red
+      , createColour 133 153   0 -- green
+      , createColour 181 137   0 -- yellow
+      , createColour  38 139 210 -- blue
+      , createColour 211  54 130 -- magenta
+      , createColour  42 161 152 -- cyan
+      , createColour 238 232 213 -- base2
+      ]
+
+    solarizedDark2 :: List8 (AlphaColour Double)
+    solarizedDark2 = fromMaybe defaultStandardColours $ mkList8
+      [ createColour   0  43  54 -- base03
+      , createColour 203  75  22 -- orange
+      , createColour  88 110 117 -- base01
+      , createColour 101 123 131 -- base00
+      , createColour 131 148 150 -- base0
+      , createColour 108 113 196 -- violet
+      , createColour 147 161 161 -- base1
+      , createColour 253 246 227 -- base3
+      ]
+
 myFontConfig :: FontConfig
 myFontConfig = FontConfig
             { fontFamily = "IBM Plex Mono Medium"
@@ -81,7 +116,7 @@ myFontConfig = FontConfig
 main :: IO ()
 main = do
   -- First, create the colour extension based on either Solarixed modules.
-  myColourExt <- createColourExtension solarizedLight
+  myColourExt <- createColourExtension solarizedDark
 
   -- Update 'myTMConfig' with our colour extension.
   let newTMConfig = addColourExtension myTMConfig myColourExt
