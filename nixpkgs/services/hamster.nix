5f705633df14455f06be1ae57ddd1c667260da2f
Róman Joost <roman@bromeco.de>
2021-05-18 18:09:37 +1000

    hamster: Allow to start services during login

--- nixpkgs/services/hamster.nix
+++ nixpkgs/services/hamster.nix
@@ -9,10 +9,12 @@
     hamster-service = {
       Unit = { Description = "Hamster time tracker D-Bus service"; };
       Service = { ExecStart = "${pkgs.hamster}/libexec/hamster/hamster-service"; };
+      Install = { WantedBy = [ "default.target" ]; };
     };
     hamster-windows-service = {
       Unit = { Description = "Hamster time tracker windows service"; };
       Service = { ExecStart = "${pkgs.hamster}/libexec/hamster/hamster-windows-service"; };
+      Install = { WantedBy = [ "default.target" ]; };
     };
   };
 }
