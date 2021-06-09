e6f1feca6a83713feca82af74cf8a0803e28d17d
Róman Joost <roman@bromeco.de>
2021-06-09 18:16:10 +1000

    emacs: completion keys where mapped the opposite

--- nixpkgs/emacs/completion.el
+++ nixpkgs/emacs/completion.el
@@ -7,8 +7,8 @@
         company-selection-wrap-around t)
 
   (define-key company-active-map [tab] 'company-complete)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous))
+  (define-key company-active-map (kbd "C-n") 'company-select-previous)
+  (define-key company-active-map (kbd "C-p") 'company-select-next))
 
 (use-package company-statistics
   :after company
