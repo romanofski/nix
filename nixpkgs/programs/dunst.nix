71f8f9a550a28fb4e2e0053c03211c0b97e38f94
Róman Joost <roman@bromeco.de>
2022-07-24 10:58:55 +1000

    Updated gnome-themes package

--- nixpkgs/programs/dunst.nix
+++ nixpkgs/programs/dunst.nix
@@ -4,7 +4,7 @@
   services.dunst = {
     enable = true;
     iconTheme = {
-      package = pkgs.gnome3.gnome_themes_standard;
+      package = pkgs.gnome3.gnome-themes-extra;
       name = "Adwaita";
     };
     settings = {
