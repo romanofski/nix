7391b198586706031239d2a3be2432d70b9b3e85
Róman Joost <roman@bromeco.de>
2022-10-07 09:33:18 +1000

    bump nixos unstable

--- nixpkgs/nixpkgsource.nix
+++ nixpkgs/nixpkgsource.nix
@@ -1,7 +1,7 @@
 {
   nixos-unstable = builtins.fetchTarball {
     # current nixos-unstable HEAD as of 13/11/2021
-    url = "https://github.com/NixOS/nixpkgs/archive/2fa862644fc15ecb525eb8cd0a60276f1c340c7c.tar.gz";
-    sha256 = "00l884zydbrww2jxjvf62sm1y96jvys22jg9vb3fsznz2mbz41jb";
+    url = "https://github.com/NixOS/nixpkgs/archive/41ff747f882914c1f8c233207ce280ac9d0c867f.tar.gz";
+    sha256 = "sha256:1przm11d802bdrhxwsa620af9574fiqsl44yhqfci0arf5qsadij";
   };
 }
