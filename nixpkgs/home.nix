235bd518f1760a92654959a5198f61596c0957be
Róman Joost <roman@bromeco.de>
2023-02-19 09:55:40 +1000

    this should be now in our channel we consume

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -21,11 +21,6 @@ in with secrets; {
         inherit (self.haskellPackages) ghcWithPackages;
       };
     })
-
-    (self: super:
-    {
-      xlockmore = super.callPackage ./overlays/packages/xlockmore.nix {};
-    })
   ];
 
   imports = [
