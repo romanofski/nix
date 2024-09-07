c34aae06c4826b4663d880e692c21ea49beb93c9
Róman Joost <roman@bromeco.de>
2024-09-07 14:26:14 +1000

    use pinentry-gnome to work with SSH

--- nixpkgs/services/gpg-agent.nix
+++ nixpkgs/services/gpg-agent.nix
@@ -4,6 +4,9 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    enableZshIntegration = true;
+    # see services.dbus.packages in system configuration too
+    pinentryPackage = pkgs.pinentry-gnome3;
     # 1year
     defaultCacheTtl = 34560000;
     maxCacheTtl = 34560000;
