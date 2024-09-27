1dd28eea03907626c7390ea693306cf4aedf6080
Róman Joost <roman@bromeco.de>
2024-09-27 17:15:16 +1000

    start in graphics boot with Plymouth

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -10,11 +10,41 @@
 
     boot = {
       initrd = {
+        verbose = false;
+        systemd.enable = true;
         availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "uas" "sd_mod" ];
         kernelModules = [ ];
       };
       kernelModules = [ "kvm-intel" ];
       extraModulePackages = [ ];
+
+      # mostly copied from https://wiki.nixos.org/wiki/Plymouth
+      plymouth = {
+        enable = true;
+        theme = "rings";
+        themePackages = with pkgs; [
+          # By default we would install all themes
+          (adi1090x-plymouth-themes.override {
+            selected_themes = [ "rings" ];
+          })
+        ];
+      };
+
+      # Enable "Silent Boot"
+      consoleLogLevel = 0;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
+      # Hide the OS choice for bootloaders.
+      # It's still possible to open the bootloader list by pressing any key
+      # It will just not appear on screen unless a key is pressed
+      loader.timeout = 0;
     };
 
     fileSystems."/" =
