5ccfb6b0b4a3ef051110398f70fe9733f96dc530
Róman Joost <romanjoost@microsoft.com>
2025-09-18 09:40:33 +1000

    stick with a stable release
    Currently unstable can't build tkinter for python3.11

--- nixpkgs/flake.nix
+++ nixpkgs/flake.nix
@@ -1,13 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    secrets.url = "git+ssh:krombopulos.lan:/home/rjoost/works/configs/nixsecrets";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    secrets.url = "git+ssh://krombopulos.lan:/home/rjoost/works/configs/nixsecrets";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-treesitter.url = "github:ratson/nix-treesitter";
   };
 
-  outputs = inputs@{ self, nixpkgs, secrets, home-manager, nix-treesitter }:
+  outputs = inputs@{ self, nixpkgs, secrets, home-manager }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
