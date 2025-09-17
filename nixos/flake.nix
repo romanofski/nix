1f4657b516714c0d9fef6b5188f2abdf4162d1a8
Róman Joost <roman@bromeco.de>
2025-09-17 19:36:11 +1000

    upgrade to 25.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
   outputs = { self, nixpkgs, nixos-hardware, ... }@attrs: {
