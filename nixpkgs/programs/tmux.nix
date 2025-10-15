40ff271c615e195bce6aed0b113fef7da53c4c35
Róman Joost <romanjoost@microsoft.com>
2025-10-16 07:19:59 +1000

    tmux: use home manager config

--- nixpkgs/programs/tmux.nix
+++ nixpkgs/programs/tmux.nix
@@ -1,5 +1,19 @@
 {pkgs, ...}:
 
 {
-  home.file.".tmux.conf".source = ./configs/tmux.conf;
+  programs.tmux = {
+    enable = true;
+    prefix = "C-a";
+    shell = "${pkgs.zsh}/bin/zsh";
+    keyMode = "vi";
+    extraConfig = ''
+unbind " "
+bind " " next-window
+unbind BSpace
+bind BSpace previous-window
+    '';
+    plugins = with pkgs; [
+      tmuxPlugins.tmux-powerline
+               ];
+  };
 }
