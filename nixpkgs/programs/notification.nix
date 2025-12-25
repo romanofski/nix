731cc61c65c4a3c56b2dc30307415cb115416290
Róman Joost <roman@bromeco.de>
2025-12-26 07:32:25 +1000

    niri: improve notifications

--- nixpkgs/programs/notification.nix
+++ nixpkgs/programs/notification.nix
@@ -7,8 +7,22 @@
     settings = {
       actions = true;
       anchor = "top-right";
-      background-color = "#000000";
-      border-color = "#FFFFFF";
+      background-color = "#303446";
+      text-color = "#c6d0f5";
+      border-color = "#babbf1";
+      progress-color = "over #414559";
+
+      "urgency=high" = {
+        border-color = "#ef9f76";
+        default-timeout = 0;
+      };
+      "urgency=normal" = {
+        default-timeout = 10;
+      };
+      "urgency=low" = {
+        default-timeout = 5;
+      };
+
       border-radius = 0;
       default-timeout = 0;
       font = "monospace 10";
