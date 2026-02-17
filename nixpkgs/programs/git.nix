988526b0671d2811970524975fb428da7e421b88
Róman Joost <romanjoost@microsoft.com>
2026-02-17 19:15:46 +1000

    git: fix warnings with config changes

--- nixpkgs/programs/git.nix
+++ nixpkgs/programs/git.nix
@@ -6,33 +6,35 @@ let
       helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
     };
   };
-  extraConfig = {
+  settings = {
+    aliases = {
+      lg = "log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative";
+      ll = "!git st; git --no-pager log --pretty='format:%Cred%cr %Cblue%h %Cgreen%an:%Creset %s%C(yellow)%d' -n20; echo";
+      # Nice list of branches by age
+      bb = "for-each-ref --sort=-committerdate --format='%1B[36m%(committerdate:relative) %1B[33m%(refname:short)%1B[0;m' refs/heads/";
+      info = "config --list";
+    };
+    user = {
+      email = "${secrets.email}";
+      name = "${secrets.realName}";
+    };
     http.version = "HTTP/1.1";
   };
-in with secrets;
+in
 {
-    enable = true;
-    aliases = {
-	    lg = "log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative";
-	    ll = "!git st; git --no-pager log --pretty='format:%Cred%cr %Cblue%h %Cgreen%an:%Creset %s%C(yellow)%d' -n20; echo";
-	    # Nice list of branches by age
-	    bb = "for-each-ref --sort=-committerdate --format='%1B[36m%(committerdate:relative) %1B[33m%(refname:short)%1B[0;m' refs/heads/";
-	    info = "config --list";
-    };
-    ignores = [
-      "dist"
-      ".cabal-sandbox"
-      "cabal.sandbox.config"
-      ".stack-work"
-      "*.o"
-      "*.hi"
-    ];
-    signing = {
-      key = "D02BC6E095A0446267E1F43C0133D0C73A765B52";
-    };
-    userEmail = "${email}";
-    userName = "${realName}";
-    extraConfig = if useGCM then extraConfig // gcm else extraConfig;
-    lfs.enable = true;
+  enable = true;
+  ignores = [
+    "dist"
+    ".cabal-sandbox"
+    "cabal.sandbox.config"
+    ".stack-work"
+    "*.o"
+    "*.hi"
+  ];
+  signing = {
+    key = "D02BC6E095A0446267E1F43C0133D0C73A765B52";
+  };
+  settings = if useGCM then settings // gcm else settings;
+  lfs.enable = true;
 }
 
